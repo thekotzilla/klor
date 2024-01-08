f87e9ee2961e5130f61cc5e79ebb33f71b871c99
thekotzilla <90445002+thekotzilla@users.noreply.github.com>
2024-01-08 21:32:26 +0300

    Klor 1.1

--- klor/src/klor/main.py
+++ klor/src/klor/main.py
@@ -1,13 +1,17 @@
 
-def kolored(message, color='0', background='0'):
+def kolored(message, color='0', background='0', bold=False, italic=False):
     messager = str(message)
 
     colors = {'0': '', 'black': '\33[30m', 'red': '\33[31m', 'green': '\33[32m', 'yellow': '\33[33m', 'blue': '\33[34m', 'purple': '\33[35m', 'white': '\33[37m'}
     backcolors = {'0': '', 'black': '\33[40m', 'red': '\33[41m', 'green': '\33[42m', 'yellow': '\33[43m', 'blue': '\33[44m', 'purple': '\33[45m', 'white': '\33[47m'}
+    ebold = {True: '\33[1m', False: ''}
+    eitalic = {True: '\33[3m', False: ''}
 
+    kbold = bold.get(bold)
+    kitalic = italic.get(italic)
     kolor = colors.get(color)
     backolor = backcolors.get(background)
-    result = backolor + kolor + messager + '\033[0m'
+    result = kbold + kitalic + backolor + kolor + messager + '\033[0m'
     return result
 
 def available_colors():
